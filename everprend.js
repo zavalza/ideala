47480acbc17b27c594ce4cd61ced19a478e03aef
Paul Zavalza <paulz_91@hotmail.com>
2014-03-23 21:50:27 -0600

    With registerForm and more templates that add functionality

--- everprend.js
+++ everprend.js
@@ -1,5 +1,16 @@
 if (Meteor.isClient) {
 
+  Template.navigation.events({
+    'click .home': function (evt, tmpl) {
+      Session.set("showRegisterForm", false);
+      return false
+    }, 
+    'click .signup' : function(evt, tmpl){
+      Session.set("showRegisterForm", true);
+      return false
+    }
+  });
+
   Template.welcome.events({
     'click .login': function (evt, tmpl) {
       Session.set("showLogin", !Session.get("showLogin"));
@@ -11,12 +22,33 @@ if (Meteor.isClient) {
     }
   });
 
+   Template.registerForm.events({
+    'click .login': function (evt, tmpl) {
+      Session.set("showLogin", !Session.get("showLogin"));
+      Session.set("showNewUser", false);
+      return false
+    },
+    'click .newUser': function (evt, tmpl) {
+      Session.set("showNewUser", !Session.get("showNewUser"));
+      Session.set("showLogin", false);
+      return false
+    }
+  });
+
   Template.welcome.showLogin = function() {
     return Session.get("showLogin");
   };
 
   Template.main.showRegisterForm = function() {
     return Session.get("showRegisterForm");
+  };
+
+ Template.registerForm.showLogin = function() {
+  return Session.get("showLogin");
+  };
+
+  Template.registerForm.showNewUser = function() {
+  return Session.get("showNewUser");
   };
 }
 
